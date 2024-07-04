ed3670f228467463644b0560b59d2fab7da7a365
Jons <ozodjon160605@gmail.com>
2024-07-04 13:03:02 +0500

    Migrate fixed

--- migrations/000001_create_users.up.sql
+++ migrations/000001_create_users.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
         user_id uuid default gen_random_uuid() PRIMARY KEY,
-        user_name VARCHAR(50) NOT NULL,
+        user_name VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
